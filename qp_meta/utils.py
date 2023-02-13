2c42d0015111e2a0f2606d03d390fc73a8e257cb
Antonio Gonzalez <antgonza@gmail.com>
2023-02-13 08:52:25 -0700

    update qiita_db.artifacts

--- qp_meta/utils.py
+++ qp_meta/utils.py
@@ -9,8 +9,6 @@
 # -----------------------------------------------------------------------------
 # This file contains functions used by multiple commands
 # -----------------------------------------------------------------------------
-import pandas as pd
-
 from qiita_client.util import system_call, get_sample_names_by_run_prefix
 from itertools import zip_longest
 from os import environ
@@ -200,13 +198,11 @@ def _per_sample_ainfo(
 
 
 def _generate_qiime_mapping_file(prep_file, out_dir):
-    df = pd.read_csv(prep_file, sep='\t', dtype='str', na_values=[],
-                     keep_default_na=False)
-    df.set_index('sample_name', inplace=True)
-
     qiime_map = f'{out_dir}/qiime-mapping-file.txt'
-    df.index.name = '#SampleID'
-    df.to_csv(qiime_map, sep='\t')
+
+    prep_file.set_index('sample_name', inplace=True)
+    prep_file.index.name = '#SampleID'
+    prep_file.to_csv(qiime_map, sep='\t')
 
     return qiime_map
 
